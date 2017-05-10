9b09d5bbe44b7f47788cff1e4b92fc1aea50b785
Taimur Shah <taimur38@gmail.com>
2017-05-09 20:42:15 -0400

    keep bin/data. avg depth

--- src/Calibrater.cpp
+++ src/Calibrater.cpp
@@ -86,6 +86,8 @@ vector<unsigned short>& Calibrater::getMappedDepthFrame(IDepthFrame** depthFrame
 	std::fill(depthBuffer.begin(), depthBuffer.end(), 0);
 	for (int i = 0; i < depthSize; i++) {
 		unsigned short depth = depthBuff[i];
+		if (i > 0 && i < depthSize - 1)
+			depth = (depthBuff[i - 1] + depthBuff[i] + depthBuff[i + 1]) / 3.0;
 		if (depth > minDepth && depth < maxDepth) {
 			int mappedIndex = convertIndex(i, depth);
 			if (mappedIndex > -1) {
--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,14 @@
 
 //========================================================================
 int main( ){
-	ofSetupOpenGL(1024,768,OF_WINDOW);			// <-------- setup the GL context
+	//ofSetupOpenGL(1024,768,OF_WINDOW);			// <-------- setup the GL context
+
+	ofGLFWWindowSettings settings;
+	settings.setGLVersion(3, 2);
+	settings.width = 1920;
+	settings.height = 1080;
+	ofCreateWindow(settings);
+	//ofSetupOpenGL(1920, 1080, OF_WINDOW);
 
 	// this kicks off the running of my app
 	// can be OF_WINDOW or OF_FULLSCREEN
